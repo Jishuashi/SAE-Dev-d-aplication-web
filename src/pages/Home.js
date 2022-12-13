a65eab67cd0cbcb6ed205b1a20272f375d0a563c
Chartier Hugo <72805472+Jishuashi@users.noreply.github.com>
2022-12-13 16:14:30 +0100

    Home page widthout responsive and minimum css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,16 @@
+import React from 'react';
+import  '../styles/home.css';
+import {Link} from "react-router-dom";
+
 function Home (){
     return(<div className="Home">
-
+        <div className="Player">
+            <iframe title="ytplayer" className="ytplayer" type="text/html" width="640" height="360"
+                    src="http://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
+                    frameBorder="0"/>
+        </div>
+        <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <Link className="linkHome" to="/inscription">S'inscrire</Link>
     </div>);
 }
 
