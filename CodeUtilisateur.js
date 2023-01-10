9508b381c10e826618beabfd827a13a9c9350b9c
suki <buibrandon47@gmail.com>
2023-01-10 19:03:38 +0100

    Ajout de la fonction convertiProba et de sa documentation. Elle permet de 'convertir' une probabilité celon t et m, et de l'arrondir à cinq chiffres après la virgule

--- CodeUtilisateur.js
+++ CodeUtilisateur.js
@@ -1,4 +1,8 @@
 window.onload = init
+let sigma = 1
+let m = 0
+let t = 1
+let arrondi = 100000.0
 
 function init(){
 
@@ -14,4 +18,17 @@ function init(){
 function loiNormal(x){
     var res = (1/(sigma*Math.sqrt(2*Math.PI)))*Math.exp(-0.5*Math.pow((x-m)/sigma,2))
     return res
+}
+
+/**
+ * Convertie la probabilité du nombre en paramètre selon t
+ *Si t>m, ajoute au nombre 0.5, sinon soustrait le nombre à 0.5
+ * @param number nombre réel positif correspondant à la probabilité entre deux points
+ * @returns {number}
+ */
+function convertiProba(number){
+    if (t>m){
+        return Math.round((number+0.5)*arrondi)/arrondi
+    }
+    return Math.round((0.5-number)*arrondi)/arrondi
 }
