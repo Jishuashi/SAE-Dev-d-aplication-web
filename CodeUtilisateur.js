fafed5a7b291cad659bd1c60ef063f15afadca16
suki <buibrandon47@gmail.com>
2023-01-10 18:47:53 +0100

    Ajout de la function loiNormal et de sa documentation

--- CodeUtilisateur.js
+++ CodeUtilisateur.js
@@ -3,3 +3,15 @@ window.onload = init
 function init(){
 
 }
+
+
+
+/**
+ * Calcule la valeur de la loi normal en x abcisse
+ * @param x nombre réel
+ * @returns {number} nombre réel
+ */
+function loiNormal(x){
+    var res = (1/(sigma*Math.sqrt(2*Math.PI)))*Math.exp(-0.5*Math.pow((x-m)/sigma,2))
+    return res
+}
