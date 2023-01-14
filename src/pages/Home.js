ef59d7bc5eac819a3eb8b04f78ec739bb600a006
darukity <samchgaming@gmail.com>
2023-01-14 21:03:19 +0100

    Changed lorem ipsum to actual text in Home.js,updated home.css, domains.css and modules.css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ function Home (){
             <div className="Player">
                 <iframe title="ytplayer" className="ytplayer" type="text/html" width="640" height="360" src="https://www.youtube.com/watch?v=gah8KMbsj40" frameBorder="0"/>
             </div>
-            <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p className="text">Si la vidéo de présentation du site vous a intrigué, pourquoi ne pas essayer par vous-même ? Inscrivez-vous dès maintenant pour pouvoir profiter de toutes les fonctionnalités du site ! </p>
         <Link className="linkHome" to="/inscription">S'inscrire</Link>
     </div>);
 }
