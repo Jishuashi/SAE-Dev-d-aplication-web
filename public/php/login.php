9a46a26b1dc56b6d3ad0e1593548bf76f5471d78
Chartier Hugo <72805472+Jishuashi@users.noreply.github.com>
2023-01-14 11:09:06 +0100

    Login Js PHP (Begin)

---
+++ public/php/login.php
@@ -0,0 +1,45 @@
+<?php
+
+//Cette ligne passe de localhost:3000 (Version de développement) à localhost:80 en version de déploiment
+header("Access-Control-Allow-Origin: http://localhost:3000");
+
+$login = $_POST['login'];
+$passwd = md5($_POST['password']);
+
+$serveur = "localhost";
+$loginDB = "sae";
+$passwdDB = "sae";
+$bd = "sae";
+
+
+$connexion = mysqli_connect($serveur, $loginDB, $passwdDB)
+or die("Connexion impossible au serveur $serveur pour $loginDB");
+
+mysqli_select_db($connexion, $bd)
+or die("Impossible d'accéder à la base de données");
+
+$request="SELECT login, password, type FROM user, passwd, permission where user.id = passwd.user_id and user.id = permission.user_id ORDER BY id";
+$result = mysqli_query($connexion, $request);
+
+$passBool = false;
+$passLog = false;
+
+while ($line=mysqli_fetch_row($result)) {
+    echo $login == $line[0];
+    if($login == $line[0]){
+        $passLog = true;
+
+        if($passwd == $line[1]){
+            $passBool = true;
+        }
+    }
+}
+
+if($passBool && $passLog){
+    echo (1);
+}else{
+    echo ("Login ou Mot de passe incorect !");
+}
+
+
+
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,27 +1,94 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles/Login.css';
-import email from '../assets/email.svg';
+import human from '../assets/human.svg';
 import lock from '../assets/lock.svg';
 import {Link} from "react-router-dom";
+import $ from "jquery";
+import {Navigate, useLocation} from "react-router";
 
 function Login() {
+
+    const [login, setLogin] = useState("");
+    const [password, setPassword] = useState("");
+    const [result , setResult] = useState("");
+
+    const handleSumbit = (e) => {
+        e.preventDefault();
+        const form = $(e.target);
+
+        $.ajax({
+            type: "POST",
+            url: form.attr("action"),
+            data: form.serialize(),
+            success(data) {
+                console.log("Connexion réussite");
+                console.log(data);
+                setResult(data);
+            },
+        });
+    };
+
+    const handleChangeLogin = (e) => {
+        setLogin(e.target.value);
+    };
+
+    const handleChangePass = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const results = () =>{
+        // eslint-disable-next-line
+
+        if(result == 1){
+            const location = useLocation();
+
+
+            /*$.ajax({
+                type: "POST",
+                url: "http://localhost:8000//isLog.php",
+                data: "script",
+                success(data) {
+                    console.log("Connexion réussite")
+                    console.log(result);
+                    //updateLog(true);
+                },
+            });*/
+
+            return(<Navigate to="/" state={{from: location}} />);
+        }
+        else {
+            return (<p>{result}</p>);
+        }
+    }
+
     return (
         <div className="card">
             <h4 className="title">Log In!</h4>
-            <form>
+            <form
+                action="http://localhost:8000/login.php"
+                method="post"
+                onSubmit={(event) => handleSumbit(event)}
+            >
                 <div className="field">
-                    <img src={email} alt="email" className="input-icon"/>
-                    <input autoComplete="off" id="logemail" placeholder="Email" className="input-field" name="logemail"
-                           type="email"/>
+                <img src={human} alt="human" className="input-icon"></img>
+                <input autoComplete="off" id="login" placeholder="Identifant" className="input-field" name="login"
+                       type="text"
+                       value={login}
+                       onInput={(event) => handleChangeLogin(event)}
+                />
                 </div>
                 <div className="field">
-                    <img src={lock} alt="lock" className="input-icon"/>
-                    <input autoComplete="off" id="logpass" placeholder="Password" className="input-field" name="logpass"
-                           type="password"/>
+                    <img src={lock} alt="lock" className="input-icon"></img>
+                    <input autoComplete="off" id="logpass" placeholder="Mot de passe" className="input-field" name="password"
+                           type="password"
+                           value={password}
+                           onInput={(event) => handleChangePass(event)}
+                    />
                 </div>
                 <button className="btn" type="submit">Login</button>
                 <Link path="/inscription" className="btn-link">Forgot your password?</Link>
             </form>
+            {results()}
             <Link className="btn-link" to="/inscription">Pas de compte ?</Link>
         </div>
     );
