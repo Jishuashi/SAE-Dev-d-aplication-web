dd478c574b1094da1bf42ac9a6f6929e48c69d37
Yanis Merabet <yanismerabet16@gmail.com>
2023-01-20 16:08:35 +0100

    Decryption of RC4 encrypted message

--- src/modules/rc4.js
+++ src/modules/rc4.js
@@ -1,24 +1,36 @@
+function checkHexa(key,str){
+    var regex = /[0-9A-Fa-f]{6}/g;
+    if (str.match(regex)){
+        return dechiffrementRC4(key,str)
+    }
+    else {
+        return rc4(key,str)
+    }
+}
+
 function rc4(key, str) {
     /**
      * @returns texte chiffré en hexadécimal
      * @param key : String
      * @param str : String
-     * @type {Uint8Array}
+     * @type {RegExp}
      */
+
 
     let ciphertext = new Uint8Array(str.length);
 
-    // Initialisation d'un array pour le chiffrement
+    // Algorithme PRGA générant la suite chiffrante de RC4
     let S = new Uint8Array(256);
     for (let i = 0; i < 256; i++) {
         S[i] = i;
     }
     let j = 0;
     for (let i = 0; i < 256; i++) {
-        j = (j + S[i] + key.charCodeAt(i % key.length)) % 256; // récupère l'unicode UTF-16 de la clé et fait un modulo 256 dessus
+        j = (j + S[i] + key.charCodeAt(i % key.length)) % 256; // Tableau construit à partir de la clé
         [S[i], S[j]] = [S[j], S[i]];
     }
 
+    // Algorithme KSA pour la génération de la permutation
     let i = 0;
     j = 0;
     for (let n = 0; n < str.length; n++) {
@@ -34,18 +46,48 @@ function rc4(key, str) {
     str = ""
     for (let i = 0 ; i < ciphertext.length ; i++){
         if (ciphertext[i] <= 16){
-           str += "0" // la conversion en hexadécimal ne renvoie pas un 0, nécessaire au bon chiffrement
+            str += "0" // la conversion en hexadécimal ne renvoie pas un 0, nécessaire au bon chiffrement
         }
         str += ciphertext[i].toString(16) // conversion décimal/hexadécimal
     }
 
     return str
 
+
 }
 
-module.exports = rc4;
+function dechiffrementRC4(key, ciphertext) {
+    // Convertit l'hexadécimal en bytes pour le déchiffrement
+    let bytes = [];
+    for (var i = 0; i < ciphertext.length; i += 2) {
+        bytes.push(parseInt(ciphertext.substr(i, 2), 16));
+    }
+    var ciphertext = String.fromCharCode.apply(null, bytes);
 
-let key = "Secret";
-let plaintext = "Attack at dawn";
-let ciphertext = rc4(key, plaintext);
-console.log(ciphertext);
+    // Algorithme PRGA générant la suite déchiffrante de RC4
+    let S = new Array(256);
+    for (let i = 0; i < 256; i++) {
+        S[i] = i;
+    }
+
+    let j = 0;
+    for (let i = 0; i < 256; i++) {
+        j = (j + S[i] + key.charCodeAt(i % key.length)) % 256;
+        [S[i], S[j]] = [S[j], S[i]];
+    }
+
+    // Algorithme KSA pour la génération de la permutation
+    var i = 0;
+    j = 0;
+    let plaintext = "";
+    for (let n = 0; n < ciphertext.length; n++) {
+        i = (i + 1) % 256;
+        j = (j + S[i]) % 256;
+        [S[i], S[j]] = [S[j], S[i]];
+        let z = S[(S[i] + S[j]) % 256];
+        plaintext += String.fromCharCode(ciphertext.charCodeAt(n) ^ z);
+    }
+    return plaintext;
+}
+
+console.log(checkHexa("Key", "bbf316e8d940af0ad3"))
--- src/pages/domains/modules/Securite/module_crypto.js
+++ src/pages/domains/modules/Securite/module_crypto.js
@@ -12,27 +12,39 @@ function ModuleCrypto({logged}) {
         t: '',
     });
 
+    function checkHexa(key,str){
+        var regex = /[0-9A-Fa-f]{6}/g;
+        if (str.match(regex)){
+            return dechiffrementRC4(key,str)
+        }
+        else {
+            return rc4(key,str)
+        }
+    }
+
     function rc4(key, str) {
         /**
          * @returns texte chiffré en hexadécimal
          * @param key : String
          * @param str : String
-         * @type {Uint8Array}
+         * @type {RegExp}
          */
+
 
         let ciphertext = new Uint8Array(str.length);
 
-        // Initialisation d'un array pour le chiffrement
+        // Algorithme PRGA générant la suite chiffrante de RC4
         let S = new Uint8Array(256);
         for (let i = 0; i < 256; i++) {
             S[i] = i;
         }
         let j = 0;
         for (let i = 0; i < 256; i++) {
-            j = (j + S[i] + key.charCodeAt(i % key.length)) % 256; // récupère l'unicode UTF-16 de la clé et fait un modulo 256 dessus
+            j = (j + S[i] + key.charCodeAt(i % key.length)) % 256; // Tableau construit à partir de la clé
             [S[i], S[j]] = [S[j], S[i]];
         }
 
+        // Algorithme KSA pour la génération de la permutation
         let i = 0;
         j = 0;
         for (let n = 0; n < str.length; n++) {
@@ -46,8 +58,8 @@ function ModuleCrypto({logged}) {
         }
 
         str = ""
-        for (let i = 0; i < ciphertext.length; i++) {
-            if (ciphertext[i] <= 16) {
+        for (let i = 0 ; i < ciphertext.length ; i++){
+            if (ciphertext[i] <= 16){
                 str += "0" // la conversion en hexadécimal ne renvoie pas un 0, nécessaire au bon chiffrement
             }
             str += ciphertext[i].toString(16) // conversion décimal/hexadécimal
@@ -55,6 +67,41 @@ function ModuleCrypto({logged}) {
 
         return str
 
+
+    }
+
+    function dechiffrementRC4(key, ciphertext) {
+        // Convertit l'hexadécimal en bytes pour le déchiffrement
+        let bytes = [];
+        for (var i = 0; i < ciphertext.length; i += 2) {
+            bytes.push(parseInt(ciphertext.substr(i, 2), 16));
+        }
+        var ciphertext = String.fromCharCode.apply(null, bytes);
+
+        // Algorithme PRGA générant la suite déchiffrante de RC4
+        let S = new Array(256);
+        for (let i = 0; i < 256; i++) {
+            S[i] = i;
+        }
+
+        let j = 0;
+        for (let i = 0; i < 256; i++) {
+            j = (j + S[i] + key.charCodeAt(i % key.length)) % 256;
+            [S[i], S[j]] = [S[j], S[i]];
+        }
+
+        // Algorithme KSA pour la génération de la permutation
+        var i = 0;
+        j = 0;
+        let plaintext = "";
+        for (let n = 0; n < ciphertext.length; n++) {
+            i = (i + 1) % 256;
+            j = (j + S[i]) % 256;
+            [S[i], S[j]] = [S[j], S[i]];
+            let z = S[(S[i] + S[j]) % 256];
+            plaintext += String.fromCharCode(ciphertext.charCodeAt(n) ^ z);
+        }
+        return plaintext;
     }
 
     const handleChange = (event) => {
@@ -68,7 +115,7 @@ function ModuleCrypto({logged}) {
         setError("")
         if (formData.key !== "") {
             if (formData.textCrypto !== "") {
-                setResult(rc4(formData.key, formData.textCrypto))
+                setResult(checkHexa(formData.key, formData.textCrypto))
             } else {
                 setError("Erreur le champ Texte n'a pas de valeur")
             }
